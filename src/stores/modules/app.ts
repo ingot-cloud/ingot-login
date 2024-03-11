1f4c9ccd284ec4c57b4cf35c319763a886b21bc1
jy <magician.of.technique@aliyun.com>
2024-03-11 11:28:37 +0800

    [*] copyright时间自动处理

--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -4,8 +4,12 @@ import type { LoginConfig } from "../types";
  * 全局配置
  */
 export const useAppStore = defineStore("app", () => {
+  const now = new Date();
   const login = reactive<LoginConfig>({
-    copyright: import.meta.env.VITE_APP_COPYRIGHT,
+    copyright: import.meta.env.VITE_APP_COPYRIGHT.replace(
+      "{0}",
+      now.getFullYear() + ""
+    ),
     loginBanner: import.meta.env.VITE_APP_LOGIN_BANNER,
     errorImage: import.meta.env.VITE_APP_ERROR_IMAGE,
     title: import.meta.env.VITE_APP_LOGIN_TITLE,
